7520804e72e31740dabd324203b6f8a7699def16
Berez85 <98259884+Berez85@users.noreply.github.com>
2022-12-05 13:11:21 +0200

    new

---
+++ js/script.js
@@ -0,0 +1,28 @@
+"use stript";
+
+console.log('Hello java script')
+console.log('Я учу JS');
+//["Учим", "JS"].forEach(alert)
+
+let myLifeStyle = 'freelance';
+myLifeStyle = 'free';
+console.log(myLifeStyle);
+
+let myAge = 24;
+console.log(myAge);
+
+function testBlock() {
+	let myAge = 37;
+	console.log(myAge);
+}
+testBlock();
+
+let brownEyes = 'карие глаза'
+console.log(brownEyes);
+
+let user = 'Вася'
+console.log(user);
+
+let userName = 'Вася'
+
+
